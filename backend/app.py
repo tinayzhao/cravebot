08a931a43384e61d9e114faefd6f70be848048f1
Tina Zhao <tinazhao@berkeley.edu>
2019-04-19 18:57:03 -0700

    Changed

--- backend/app.py
+++ backend/app.py
@@ -3,7 +3,6 @@
 #send request back to frontend
 import sqlite3
 import json
-import db as db
 import yelp_api as yelp_api
 
 from flask import Flask, request, jsonify
@@ -20,28 +19,14 @@ def get_messages():
     return jsonify({'error':'no user found'})
 '''
 
-@app.route('api/backend', methods = ['POST'])
+@app.route('/api/backend', methods = ['POST'])
 def main():
     json = request.get_json()
     #Jennifer's JSON {Location: Berkeley}
     #{Message Type: "Location", Message: "Berkeley"}
     #Message type: Location, Category, Extra: [Price, Category]
-    yelp_call(json['Location'], json['Category1'], json['Category2'], json['Price'])
-    return jsonify()
-
-@app.route('/api/get_messages', methods = ['POST'])
-def main():
-    json = request.get_json()
-    json_keys = json.keys()
-	#store json in database
-	#if nothing, will change once i see the json format:
-
-    if json is None:
-        return flask.json.jsonify(messages =  "Hi. I'm Cravebot. <3 What are you craving today?")
-        return flask.json.jsonify({'messages': "Hi. I'm Cravebot. <3 What are you craving today?"})
-    else:
-        #return message if location is missing
-
+    #yelp_call(json['Location'], json['Category1'], json['Category2'], json['Price'])
+    return jsonify(yelp_call(json['Location'], json['Category'], json['Price']))
 
 
     #else:
@@ -51,22 +36,6 @@ def main():
 #@app.route('/test', methods = ['GET'])
 #def test():
 #    return jsonify(message = "hello world")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 if __name__ == '__main__':
