edebfdbfba0e178c780667621761d53110e7f016
David Kang <davidhjkang@berkeley.edu>
2019-04-24 21:35:52 -0700

    added conversation logic, hardcoded questions to ask, harded error messages

--- craveBot/craveBot/ViewControllers/TempChatBotViewController.swift
+++ craveBot/craveBot/ViewControllers/TempChatBotViewController.swift
@@ -15,13 +15,23 @@ import CoreLocation
 class TempChatBotViewController: UIViewController, CLLocationManagerDelegate {
     
     var chefAnimation: AnimationView?
-    
+
     @IBOutlet var craveBotText: CLTypingLabel!
     @IBOutlet var input: UITextField!
     @IBOutlet var sendButton: UIButton!
     
+    // the three pieces of information for the search
+    var userLocation: String?
+    var foodType: String?
+    var foodPrice: String?
+    
     let manager = CLLocationManager()
     
+    // 0 means we're on first question
+    // 1 means we're on the second question
+    // 2 means we're on the third question
+    
+    var currentQuestion = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -34,6 +44,7 @@ class TempChatBotViewController: UIViewController, CLLocationManagerDelegate {
         
         let userLocation: CLLocation? = manager.location
         
+        // will set the input to be current location if Core Location is allowed
         if (userLocation != nil) {
             CLGeocoder().reverseGeocodeLocation(userLocation!) { (CLPlacemark, error) in
                 if error != nil {
@@ -62,25 +73,48 @@ class TempChatBotViewController: UIViewController, CLLocationManagerDelegate {
     
     override func viewDidAppear(_ animated: Bool) {
        
-        // NOT CORRECT IMPLEMENTATION!!! but makes animation loop 3 times
-        chefAnimation?.play { (finished) in
-            self.chefAnimation?.play() {(finished) in self.chefAnimation?.play()}
-        }
+        chefwiggle()
+        craveBotText.text = "Hi there! I'm Cravebot\n Where would you like\n to eat today?"
         
-        craveBotText.text = "Hi there! I'm Cravebot\n Where would you like\nto eat today?"
     }
     
     
     @IBAction func sendToBot(_ sender: Any) {
         
-        // NOT CORRECT IMPLEMENTATION!!! but makes animation loop 3 times
+        // send the information to backend and get returned json
+        
+        // if (returned json is nil or has error message)
+        
+            // sayBadInput()
+            // askQuestion(n: self.currentQuestion)
+            // clear user input
+        // else:
+            // store the json information so we can prepare to segue if they swipe
+            // self.currentQuestion += 1
+            // clear user input
+    }
+    
+    func askQuestion(_ n: Int) {
+        if n == 0 {
+            craveBotText.text = "Okay! Where would you \n like to eat today?"
+        } else if n == 1 {
+            craveBotText.text = "What kind of food are you craving?"
+        } else if n == 2 {
+            craveBotText.text = "How much are you looking to spend?"
+        }
+    }
+    
+    func sayBadInput() {
+        craveBotText.text = "Sorry, I don't understand."
+    }
+    
+    
+    // Not Ideal Implementation! but makes animation loop 3 times
+    func chefwiggle() {
         chefAnimation?.play { (finished) in
             self.chefAnimation?.play() {(finished) in self.chefAnimation?.play()}
         }
         
-        input.text = ""
-        craveBotText.text = "Okay! Where would you \n like to eat today?"
-    
     }
     
     
